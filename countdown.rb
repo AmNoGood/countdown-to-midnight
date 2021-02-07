d6afd80eb23bca6f01270c1c67788b3e434432d6
Juan <Juan.Hoyos@lc.cuny.edu>
2021-02-07 01:31:53 -0500

    commit tree aa0ae44803362e8232804c145b7881929572482d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
-
 def countdown
+    number = 20
+    while number >= 1
+        number -= 1
+        puts "#{number} SECOND(S)!"
+        puts "HAPPY NEW YEAR!"
+    end
 end
+
+def countdown_with_sleep
